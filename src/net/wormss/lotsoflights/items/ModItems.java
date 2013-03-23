1e66fb0b2ef1a01e8eef6b6b1b402b89a206caad
WORMSS <github@WORMSS.net>
2013-03-23 02:52:28 +0000

    Cleaned up ModItems.

--- src/net/wormss/lotsoflights/items/ModItems.java
+++ src/net/wormss/lotsoflights/items/ModItems.java
@@ -9,20 +9,9 @@ public class ModItems
 	public static final Map<String, Integer> ids = new HashMap<String,Integer>(){{
 		
 	}};
-
-
-	private static boolean created;
-	public static void create()
-	{
-		
-		created = true;
-	}
 	
 	public static void register()
 	{
-		if (!created)
-		{
-			create();
-		}
+		
 	} 
 }
